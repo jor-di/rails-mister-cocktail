d580d1a638b845263c56deceef4c33460b44ac54
jordi Tabit <jordi.tabit@hotmail.fr>
2019-02-14 12:54:30 +0100

    Model is ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'ingredients/index'
+  get 'ingredients/show'
+  get 'ingredients/new'
+  get 'ingredients/create'
+  get 'ingredients/update'
+  get 'ingredients/destroy'
+  get 'ingredients/edit'
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
+  get 'cocktails/update'
+  get 'cocktails/destroy'
+  get 'cocktails/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
